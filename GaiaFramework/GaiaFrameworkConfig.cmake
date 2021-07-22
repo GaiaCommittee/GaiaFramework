9c93256e5507c393237a26ef316a18d952be28c8
Vincent <rumble.vecent@outlook.com>
2021-07-22 22:08:11 +0800

    Change install script and add version support.

--- GaiaFramework/GaiaFrameworkConfig.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(3.10)
-
-find_path(GaiaFramework_INCLUDE_DIRS "GaiaFramework")
-find_library(GaiaFramework_LIBS "GaiaFramework")
-
-if (GaiaFramework_INCLUDE_DIRS AND GaiaFramework_LIBS)
-    set(GaiaFramework_FOUND TRUE)
-endif()
